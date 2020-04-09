48d9e3483b6abd24ca27091d9e3b4a147c13b28e
sumanth82 <sumanth82@gmail.com>
2020-04-09 18:50:15 -0400

    Initial code

--- Hello.py
+++ Hello.py
@@ -14,3 +14,13 @@ import os
 print('Hello')
 print('Hello Python')
 
+
+my_str='This is a string'
+print(my_str)
+
+my_num=8
+print(my_num)
+print(type(my_num))
+
+full_name='Sumant'+'\nRenukarya'
+print(full_name.lower())
