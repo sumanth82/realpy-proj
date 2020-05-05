431afea49c5809f287061ec1799a4d09715a8b54
sumanth82 <sumanth82@gmail.com>
2020-05-04 23:14:19 -0400

    Changes

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+# if and else if 
+
+if 'a' < 'b':
+    print('Condition was true')
+
+if ('1' < '2'):
+    print('Maybe 1 is less than 2')
+
+if (10>20):
+    print('10 is NOT GREATER THAN 20')
+else:
+    print('10 is LESS THAN 20')
+
+
