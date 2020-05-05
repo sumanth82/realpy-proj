cb8905d989d2a1956b5450f9fad7c47ceec443ee
sumanth82 <sumanth82@gmail.com>
2020-05-04 23:31:36 -0400

    Changes

--- conditionals.py
+++ conditionals.py
@@ -11,4 +11,38 @@ if (10>20):
 else:
     print('10 is LESS THAN 20')
 
+###########  ELIF #####################
+
+
+if 'b' < 'a':
+    print('Test a')
+elif 'b' > 'a':
+    print('Test b')  # O/P: Test b
+else:
+    print('Dead End')
+
+
+####
+
+x = input('What is your Name?: ')
+
+print(x)
+
+if len(x) > 6:
+    print('Name length exceeded')
+elif len(x) < 2:
+    print('Name length is too short')
+else:
+    print('Wonderful Name')
+
+############# PASS ##########
+
+
+name = 'Google'
+
+if name == 'Google':
+    print('apple')
+else:
+    pass
+
 
